84419ec366ed0f7d4ce130296d2d2ccc9034e3cb
Jordan Ridenour <jordandr@umich.edu>
2016-10-17 23:30:48 -0400

    started canvas feature for new diagram

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,32 @@
-// Front-end script for index page
+// Front-end script for navigation controls
 const {ipcRenderer} = require('electron');
 
+document.querySelector('#home').addEventListener('click', function () {
+  if(location.href.split("/").slice(-1) != 'index.html') {
+    ipcRenderer.send('changeWindow', 'index.html');
+  }
+});
+
 document.querySelector('#about').addEventListener('click', function () {
-  ipcRenderer.send('changeWindow', 'about.html');
+  if(location.href.split("/").slice(-1) != 'about.html') {
+    ipcRenderer.send('changeWindow', 'about.html');
+  }
 });
 
 document.querySelector('#help').addEventListener('click', function () {
-  ipcRenderer.send('changeWindow', 'help.html');
+  if(location.href.split("/").slice(-1) != 'help.html') {
+    ipcRenderer.send('changeWindow', 'help.html');
+  }
 });
 
 document.querySelector('#loadDiagram').addEventListener('click', function () {
-  ipcRenderer.send('changeWindow', 'loadDiagram.html');
+  if(location.href.split("/").slice(-1) != 'loadDiagram.html') {
+    ipcRenderer.send('changeWindow', 'loadDiagram.html');
+  }
 });
 
 document.querySelector('#newDiagram').addEventListener('click', function () {
-  ipcRenderer.send('changeWindow', 'newDiagram.html');
+  if(location.href.split("/").slice(-1) != 'newDiagram.html') {
+    ipcRenderer.send('changeWindow', 'newDiagram.html');
+  }
 });
