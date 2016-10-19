bd661b79af5bd1f7a839f0dea13c0730f67c489d
Jordan Ridenour <jordandr@umich.edu>
2016-10-18 22:03:42 -0400

    added buttons for changing x-y coordinates of lines

--- js/newDiagram.js
+++ js/newDiagram.js
@@ -1,2 +1,10 @@
 // Front-end script for canvas controls
 const {ipcRenderer} = require('electron');
+
+function addImage(x) {
+  var canvas = $('#fbdCanvas')[0];
+  context = canvas.getContext('2d');
+  var img = new Image();
+  img.src = x
+  context.drawImage(img, canvas.width/2 - 75/2, canvas.height/2 - 75/2, 75, 75)
+}
